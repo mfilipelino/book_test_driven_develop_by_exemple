93e1e5ed5b7638d664fb50ea609b83d7799f5068
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-09 15:10:29 -0300

    add test equality

--- dollar.py
+++ dollar.py
@@ -2,12 +2,18 @@
 
 class Dollar(object):
 
-    def __init__(self, value):
-        self._amount = value
+    def __init__(self, amount):
+        self._amount = amount
 
-    def times(self, value):
-        self._amount *= value
+    def times(self, multiple):
+        return Dollar(self._amount * multiple)
 
     @property
     def amount(self):
         return self._amount
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- test_dollar.py
+++ test_dollar.py
@@ -5,6 +5,13 @@ from dollar import Dollar
 class TestDollar(TestCase):
 
     def test_multiplication(self):
-        five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        dollar_5 = Dollar(5)
+        dollar_10 = dollar_5.times(2)
+        self.assertEqual(10, dollar_10.amount)
+        dollar_15 = dollar_5.times(3)
+        self.assertEqual(15, dollar_15.amount)
+
+    def test_equality(self):
+        d1 = Dollar(5)
+        d2 = Dollar(5)
+        self.assertEqual(d1, d2)
