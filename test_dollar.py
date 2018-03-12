98c50cbc78542509c3bb842759c62910ae5ee70d
Marcos Filipe Lino <mfilipelino@gmail.com>
2018-03-12 07:10:05 -0300

    add test_dollar

--- test_dollar.py
+++ test_dollar.py
@@ -5,13 +5,14 @@ from dollar import Dollar
 class TestDollar(TestCase):
 
     def test_multiplication(self):
-        dollar_5 = Dollar(5)
-        dollar_10 = dollar_5.times(2)
-        self.assertEqual(10, dollar_10.amount)
-        dollar_15 = dollar_5.times(3)
-        self.assertEqual(15, dollar_15.amount)
+        dollar_five = Dollar(5)
+        self.assertEqual(dollar_dive, Dollar(10))
+        dollar_c = dollar_a.times(3)
+        self.assertEqual(Dollar(15), dollar_c)
 
     def test_equality(self):
         d1 = Dollar(5)
         d2 = Dollar(5)
+        d3 = Dollar(6)
         self.assertEqual(d1, d2)
+        self.assertNotEqual(d1, d3)
